7f06c5519cd75f3dbf198ac1da25d4d29b178162
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2018-12-18 18:30:13 -0600

    Update netmiko6.py

--- Python-for-networking/netmiko6.py
+++ Python-for-networking/netmiko6.py
@@ -5,35 +5,35 @@ from netmiko import ConnectHandler
 iosv_l2_s1 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.71',
-    'username': 'david',
+    'username': 'dushyant',
     'password': 'cisco',
 }
 
 iosv_l2_s2 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.72',
-    'username': 'david',
+    'username': 'dushyant',
     'password': 'cisco',
 }
 
 iosv_l2_s3 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.73',
-    'username': 'david',
+    'username': 'dushyant',
     'password': 'cisco',
 }
 
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.74',
-    'username': 'david',
+    'username': 'dushyant',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.75',
-    'username': 'david',
+    'username': 'dushyant',
     'password': 'cisco',
 }
 
